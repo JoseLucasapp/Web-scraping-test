74edcabcc5b1b8cd6c035aff7353705a5498b005
José Lucas <josluc2016@gmail.com>
2022-08-26 02:00:39 -0300

    fix: Alterações feitas no dia 18 de agosto, porém não percebi que tinha deixado no meu pc e não subi no repositório, acho que estava um pouco ansioso para a entrega. Mil perdões.

--- src/models/notebooks.js
+++ src/models/notebooks.js
@@ -17,22 +17,46 @@ export const getNotebooksData = async () => {
 
     const data = await evalPage(page)
 
-    const result = checkParams(data)
+    const results = checkParams(data)
+
+    for (let i = 0; i < results.length; i++) {
+        const result = results[i]
+        await page.goto(result.link)
+
+        const allOptions = await page.evaluate(() => {
+            const allPriceButtons = document.querySelectorAll("div.swatches > button");
+
+            const allOptionsDetails = [];
+            for (let o = 0; o < allPriceButtons.length; o++) {
+                const button = allPriceButtons[o]
+                button.click();
+                allOptionsDetails.push({
+                    price: document.querySelector("div.caption > h4.price").innerHTML,
+                    hdd: button.value,
+                    disabled: button.classList.contains("disabled"),
+                });
+            }
+            return allOptionsDetails;
+        });
+
+        results[i] = { ...result, allOptions };
+    }
 
     await browser.close();
 
-    return { data: result, total: result.length }
+    return { data: results, total: results.length }
 }
 
 const evalPage = async (page) => await page.$$eval('.test-site > .row > .col-md-9 > .row > .col-sm-4',
     (divs) => divs.map((div) => {
         const imageUrl = div.querySelector('div.thumbnail img').src
+        const link = div.querySelector('div.thumbnail div.caption h4 a').href
         const description = div.querySelector('.description').innerHTML
         const title = div.querySelector('.title').title
         const price = parseFloat((div.querySelector('h4.pull-right').innerHTML).replace('$', ''))
         const reviews = parseInt((div.querySelector('p.pull-right').innerHTML).replace('reviews', ''))
         const ratings = div.querySelector('div.ratings :nth-child(2)').childElementCount
 
-        return { title, description, price, reviews, ratings, imageUrl }
+        return { title, link, description, price, reviews, ratings, imageUrl }
     })
 )
